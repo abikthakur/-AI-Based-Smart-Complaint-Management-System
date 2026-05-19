200b4493246c738dcc59478d916a18d73e8b1335
abikthakur <abikthakur05@gmail.com>
2026-05-19 11:49:16 +0530

    fix: added root route

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/complaints', require('./routes/complaintRoutes'));
